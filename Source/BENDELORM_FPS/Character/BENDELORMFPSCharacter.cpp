cbb22c753f33f5b83437c15860e595edc4081e6c
Bendelorm <269416@stud.inn.no>
2024-01-18 14:58:17 +0100

    big push
    Deleted files and generated new project

---
+++ Source/BENDELORM_FPS/Character/BENDELORMFPSCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BENDELORM_FPS/Character/BENDELORMFPSCharacter.h"
+
+// Sets default values
+ABENDELORMFPSCharacter::ABENDELORMFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABENDELORMFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABENDELORMFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABENDELORMFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
