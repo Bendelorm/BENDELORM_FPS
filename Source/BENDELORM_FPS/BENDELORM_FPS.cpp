ab6f21c321db990235011eb134995e42fc44559d
Bendelorm <269416@stud.inn.no>
2024-01-18 14:23:17 +0100

    Commit
    Commitlol

---
+++ Source/BENDELORM_FPS/BENDELORM_FPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BENDELORM_FPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BENDELORM_FPS, "BENDELORM_FPS" );
