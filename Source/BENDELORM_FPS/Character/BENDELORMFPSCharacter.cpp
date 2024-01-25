c2beb71834aa46bc9b520a8831485417266a012f
Bendelorm <269416@stud.inn.no>
2024-01-25 13:50:04 +0100

    skltlmeshcomponent
    Added skeletalmeshcomponent for character

--- Source/BENDELORM_FPS/Character/BENDELORMFPSCharacter.cpp
+++ Source/BENDELORM_FPS/Character/BENDELORMFPSCharacter.cpp
@@ -8,7 +8,7 @@ ABENDELORMFPSCharacter::ABENDELORMFPSCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	SKFPV = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPSMesh"));
 }
 
 // Called when the game starts or when spawned
