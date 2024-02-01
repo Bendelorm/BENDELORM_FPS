46dd6a5189b70aa12feb889137eaeb090cbbbf1d
Bendelorm <269416@stud.inn.no>
2024-02-01 14:59:46 +0100

    Movement and Camera
    Added WASD, SPACE for jump, and camera controls

--- Source/BENDELORM_FPS/Character/BENDELORMFPSCharacter.cpp
+++ Source/BENDELORM_FPS/Character/BENDELORMFPSCharacter.cpp
@@ -1,21 +1,67 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "BENDELORM_FPS/Character/BENDELORMFPSCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputAction.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABENDELORMFPSCharacter::ABENDELORMFPSCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	//Camera Component
+	FPCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	FPCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FPCameraComponent->bUsePawnControlRotation = true;
+
+	//Skeletal Mesh Component
 	SKFPV = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPSMesh"));
+	SKFPV->SetupAttachment(FPCameraComponent);
 }
+
 
 // Called when the game starts or when spawned
 void ABENDELORMFPSCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//Adding the Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(IMC, 0);
+		}
+	}
 	
+}
+
+void ABENDELORMFPSCharacter::Move(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Yellow, TEXT("Triggering move action"));
+
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if(Controller != nullptr)
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.X);
+		AddMovementInput(GetActorRightVector(), MovementVector.Y);
+	}
+}
+
+void ABENDELORMFPSCharacter::LookAround(const FInputActionValue& Value)
+{
+	FVector2D LookAroundVector = Value.Get<FVector2D>();
+
+	if(Controller != nullptr)
+	{
+		AddControllerYawInput(LookAroundVector.X);
+		AddControllerPitchInput(LookAroundVector.Y);
+	}
 }
 
 // Called every frame
@@ -28,7 +74,15 @@ void ABENDELORMFPSCharacter::Tick(float DeltaTime)
 // Called to bind functionality to input
 void ABENDELORMFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
 
+	if(EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABENDELORMFPSCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABENDELORMFPSCharacter::LookAround);
+
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+	}
 }
 
--- Source/BENDELORM_FPS/Character/BENDELORMFPSCharacter.h
+++ Source/BENDELORM_FPS/Character/BENDELORMFPSCharacter.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EnhancedInputSubsystemInterface.h"
 #include "GameFramework/Character.h"
 #include "BENDELORMFPSCharacter.generated.h"
+
+struct FInputActionValue;
+class UCameraComponent;
+class UInputComponent;
+class UInputAction;
+class UInputMappingContext;
 
 UCLASS()
 class BENDELORM_FPS_API ABENDELORMFPSCharacter : public ACharacter
@@ -15,17 +22,39 @@ public:
 	// Sets default values for this character's properties
 	ABENDELORMFPSCharacter();
 
-	USkeletalMeshComponent* SKFPV;
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	//Components
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UCameraComponent* FPCameraComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USkeletalMeshComponent* SKFPV;
+
+	//Input Mapping Context and Actions
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* IMC;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookAction;
+
+	void Move(const FInputActionValue& Value);
+	void LookAround(const FInputActionValue& Value);
+
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
